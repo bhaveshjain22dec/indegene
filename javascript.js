99f0f4a249bddf928b32ada0019660f6a719c51f
bhaveshjain22 <bhaveshjain22dec@gmail.com>
2021-02-17 23:12:48 +0530

    upload the initial code

---
+++ javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').val('1');
+  
+      $('.setValue').click(function(){
+       
+           
+          $('.slider').val('1');
+          
+      });
+  });
